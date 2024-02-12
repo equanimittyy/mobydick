d58d7f837ca692f17323fb75ed1c208b185f4a80
equanimittyy <stephennguyen@live.com.au>
2024-02-13 09:36:08 +1100

    updates regarding scraping of data

--- ScrapeLocal.py
+++ ScrapeLocal.py
@@ -1,6 +1,7 @@
 # Read and import local HTML
 print('Importing required modules')
 from bs4 import BeautifulSoup
+import csv
 
 print ('Reading file')
 with open ('theSoup.html', encoding='utf-8') as file:
@@ -16,6 +17,7 @@ title = soup.find('h1').get_text()
 h2_elementlist = soup.find_all('h2')
 print (title)
 
+# Write to .txt to test the formatting
 with open ('Moby-Dick.txt','w',encoding='utf-8') as file:
     file.write(title)
     for element in h2_elementlist:
@@ -25,7 +27,26 @@ with open ('Moby-Dick.txt','w',encoding='utf-8') as file:
                 break
             else:
                 file.write(tag.get_text(strip=True, separator=' '))
-print('Moby-Dick file has been transcribed')
+print('Moby-Dick transcript has been transcribed')
+
+# Write to .csv
+print ('Writing csv file...')
+with open ('MobyTable.csv','w',encoding='utf-8',newline='') as mobTable:
+    csvwriter = csv.writer(mobTable)
+    field = ['Header','Content']
+    
+    #Write header
+    csvwriter.writerow(field)
+    
+    for element in h2_elementlist:
+        for tag in element.next_siblings:
+            if tag.name == 'h2':
+                break
+            else:
+                csvwriter.writerow({element.get_text(),tag.get_text()})
+
+    print ('MobyTable exported')
+        
             
 
 
